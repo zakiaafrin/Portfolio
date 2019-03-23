c2a597f3ad536fce636e0531b9cd15449b5dcac4
Zakia Afrin Jeme <zjeme@techlaunch.io>
2019-03-23 19:01:52 -0400

    Updated

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'trendytheme.net@gmail.com';  // put your email here
+    $to         = 'zakia_afrin@yahoo.com';  // put your email here
 
     $email_template = 'simple.html';
 
